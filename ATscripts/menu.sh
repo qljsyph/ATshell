4ebd4dca010ad988536d716efd2124f6eedd7474
qljshyph <165464963+qljsyph@users.noreply.github.com>
2025-02-09 18:15:39 +0800

    v1.10.3

--- ATscripts/menu.sh
+++ ATscripts/menu.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# 禁用输出缓冲
-stdbuf -o0 bash
-
 BASE_URL="https://ghfast.top/https://raw.githubusercontent.com/qljsyph/ATAsst/refs/heads/main/ATscripts"
 SCRIPTS_DIR="/etc/mihomo/scripts"
 
